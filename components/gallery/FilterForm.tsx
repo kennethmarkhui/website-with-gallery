a848a9ee2662a5e9cf7b5e18457be8d50035131d
kennethmarkhui <kennethmarkhui@gmail.com>
2023-01-24 18:23:00 +0800

    added the ability to collapse category checkbox and set search and category filter input value from the query string

--- components/gallery/FilterForm.tsx
+++ components/gallery/FilterForm.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef, useState } from 'react'
+import { ComponentPropsWithoutRef, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { type Category } from 'prisma/prisma-client'
 import {
@@ -7,7 +7,8 @@ import {
   UseControllerProps,
   useForm,
 } from 'react-hook-form'
-import { HiOutlineSearch } from 'react-icons/hi'
+import { Disclosure, Transition } from '@headlessui/react'
+import { HiOutlineSearch, HiChevronDown } from 'react-icons/hi'
 
 import { type GalleryFilters } from 'types/gallery'
 import useCategory from 'hooks/gallery/category/useCategory'
@@ -20,17 +21,19 @@ interface FilterFormProps extends ComponentPropsWithoutRef<'form'> {
 
 interface CategoryCheckboxesProps extends UseControllerProps {
   options: Pick<Category, 'name' | 'id'>[]
+  selected?: string[]
 }
 
 // https://github.com/react-hook-form/react-hook-form/issues/9248#issuecomment-1284588424
 const CategoryCheckboxes = ({
   options,
+  selected,
   control,
   name,
 }: CategoryCheckboxesProps): JSX.Element => {
   const { field } = useController({ control, name })
   const [checkedCategories, setCheckedCategories] = useState(
-    field.value || new Set()
+    new Set(selected) || new Set(field.value)
   )
 
   const handleOnChange = (categoryName: string) => {
@@ -55,6 +58,7 @@ const CategoryCheckboxes = ({
             <input
               id={newId}
               type="checkbox"
+              checked={checkedCategories.has(name)}
               className="cursor-pointer rounded border-gray-300 text-black transition focus:ring-0 focus:ring-offset-0 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75"
               onChange={() => handleOnChange(name)}
             />
@@ -77,12 +81,25 @@ const FilterForm = ({
   className,
 }: FilterFormProps): JSX.Element => {
   const router = useRouter()
-  const { register, handleSubmit, control } = useForm<GalleryFilters>({
+  const { search, categories } = router.query
+
+  const { register, handleSubmit, reset, control } = useForm<GalleryFilters>({
     defaultValues: { search: '', categories: [] },
   })
 
   //   TODO: data somehow returns empty array instead of undefined as defined in the type
   const { data, status, error } = useCategory()
+
+  useEffect(() => {
+    if (!search && !categories) return
+    reset({
+      search: typeof search === 'string' ? search : undefined ?? '',
+      categories:
+        typeof categories === 'string'
+          ? categories.split(',')
+          : undefined ?? [],
+    })
+  }, [search, categories, reset])
 
   const onSubmit: SubmitHandler<GalleryFilters> = (data) => {
     // filter out falsy and empty arrays
@@ -117,11 +134,39 @@ const FilterForm = ({
         {status === 'loading' && <p>loading</p>}
         {status === 'error' && <p>error</p>}
         {status === 'success' && data && (
-          <CategoryCheckboxes
-            options={data}
-            control={control}
-            name="categories"
-          />
+          <Disclosure defaultOpen>
+            {({ open }) => (
+              <>
+                <Disclosure.Button className="flex w-full items-center justify-between text-sm text-gray-500">
+                  <span>Category</span>
+                  <HiChevronDown
+                    className={`${open ? 'rotate-180 transform' : ''}`}
+                  />
+                </Disclosure.Button>
+                <Transition
+                  enter="transition duration-100 ease-out"
+                  enterFrom="transform scale-95 opacity-0"
+                  enterTo="transform scale-100 opacity-100"
+                  leave="transition duration-75 ease-out"
+                  leaveFrom="transform scale-100 opacity-100"
+                  leaveTo="transform scale-95 opacity-0"
+                >
+                  <Disclosure.Panel className="py-2">
+                    <CategoryCheckboxes
+                      options={data}
+                      selected={
+                        typeof categories === 'string'
+                          ? categories.split(',')
+                          : undefined
+                      }
+                      control={control}
+                      name="categories"
+                    />
+                  </Disclosure.Panel>
+                </Transition>
+              </>
+            )}
+          </Disclosure>
         )}
       </fieldset>
     </form>
