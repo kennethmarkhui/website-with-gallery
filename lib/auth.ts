ef5afa83615032917d6b9035150d89ae9e71225f
kennethmarkhui <kennethmarkhui@gmail.com>
2023-04-13 22:31:41 +0800

    moved authOptions to a seperate file

---
+++ lib/auth.ts
@@ -0,0 +1,39 @@
+import { NextAuthOptions } from 'next-auth'
+import EmailProvider from 'next-auth/providers/email'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+
+import { prisma } from 'lib/prisma'
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  // Configure one or more authentication providers
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+      maxAge: 1 * 60 * 60, // 1 hour
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+    maxAge: 1 * 60 * 60, // 1 hour
+  },
+  pages: {
+    signIn: '/auth/signin',
+    error: '/auth/error',
+  },
+
+  callbacks: {
+    // https://next-auth.js.org/tutorials/role-based-login-strategy
+    async session({ session, token }) {
+      if (token.role) session.user.role = token.role
+      return session
+    },
+    async jwt({ token, user }) {
+      if (user?.role) token.role = user.role
+      return token
+    },
+  },
+}
